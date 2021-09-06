06ddbe314119d219136205feb01ab734a328c3de
Natalie Gonçalves <nataliegoncalvesdasilva02@gmail.com>
2021-09-05 23:17:53 -0300

    resolução do exercício memetonervoso

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace memetonervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasedigitada, frasememetonervoso;
+            Console.WriteLine("O que você acha de mim? ");
+            frasedigitada = Console.ReadLine();
+
+            frasememetonervoso = $"{frasedigitada.Remove(10)}... NÃO NÃO, PERA. TÔ NERVOSO";
+
+            Console.WriteLine(frasememetonervoso);
+        }
+    }
+}
